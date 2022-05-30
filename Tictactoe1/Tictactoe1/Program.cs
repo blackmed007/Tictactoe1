cdad67c2810dfe6d4f5b7d4f3731f156c75eade5
blackmed007 <92275188+blackmed007@users.noreply.github.com>
2022-05-30 15:19:41 +0200

    Add files via upload

---
+++ Tictactoe1/Tictactoe1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+class tic_tac_toe
+{
+
+    static void Main()
+    {
+        char[] position_array = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        Console.WriteLine("Welcome to tic-tac-toe!");
+        Console.WriteLine("\t|\t|");
+        Console.WriteLine("  - - - + - - - + - - - ");
+        Console.WriteLine("\t|\t|");
+        Console.WriteLine("  - - - + - - - + - - - ");
+        Console.WriteLine("\t|\t|");
+        for (int i = 1; i <= 9; i++)
+        {
+            int index = 0;
+        jump1:
+        jump2:
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9)
+            {
+                Console.Write("X's move >");
+                index = Convert.ToInt32(Console.ReadLine());
+                if (index < 0 || index > 9)
+                {
+                    Console.WriteLine("Illgel move! try again.");
+                    goto jump1;
+                }
+
+                if (index >= 1 && index <= 9)
+                {
+
+                    if (position_array[index - 1] == ' ')
+                    {
+                        position_array[index - 1] = 'X';
+                        Console.WriteLine("     |     |      ");
+                        Console.WriteLine("  {0}  |  {1}  |  {2}", position_array[0], position_array[1], position_array[2]);
+                        Console.WriteLine("_ _ _+_ _ _+ _ _ _ ");
+                        Console.WriteLine("     |     |      ");
+                        Console.WriteLine("  {0}  |  {1}  |  {2}", position_array[3], position_array[4], position_array[5]);
+                        Console.WriteLine("_ _ _+_ _ _+ _ _ _ ");
+                        Console.WriteLine("     |     |      ");
+                        Console.WriteLine("  {0}  |  {1}  |  {2}", position_array[6], position_array[7], position_array[8]);
+                        Console.WriteLine("     |     |      ");
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illgel move! try again.");
+                        goto jump1;
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("O's move >");
+                index = Convert.ToInt32(Console.ReadLine());
+                if (index < 0 || index > 9)
+                {
+                    Console.WriteLine("Illgel move! try again.");
+                    goto jump2;
+                }
+                if (index >= 1 && index <= 9)
+                {
+
+                    if (position_array[index - 1] == ' ')
+                    {
+                        position_array[index - 1] = 'O';
+                        Console.WriteLine("     |     |      ");
+                        Console.WriteLine("  {0}  |  {1}  |  {2}", position_array[0], position_array[1], position_array[2]);
+                        Console.WriteLine("_ _ _+_ _ _+ _ _ _ ");
+                        Console.WriteLine("     |     |      ");
+                        Console.WriteLine("  {0}  |  {1}  |  {2}", position_array[3], position_array[4], position_array[5]);
+                        Console.WriteLine("_ _ _+_ _ _+ _ _ _ ");
+                        Console.WriteLine("     |     |      ");
+                        Console.WriteLine("  {0}  |  {1}  |  {2}", position_array[6], position_array[7], position_array[8]);
+                        Console.WriteLine("     |     |      ");
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illgel move! try again.");
+                        goto jump2;
+                    }
+
+                }
+            }
+        }
+        Console.WriteLine("Game Over!");
+    }
+}
